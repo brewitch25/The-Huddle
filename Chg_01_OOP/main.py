1168edcbac82c7d02fd0374a54c02da4c45f501e
Vanina Maldonado <vanimaldo.vm@gmail.com>
2026-05-20 12:07:41 -0300

    Se agrego la clase UIConsole, que maneja la validacion del tamaño del tablero, usando try except

--- Chg_01_OOP/main.py
+++ Chg_01_OOP/main.py
@@ -1,1 +1,19 @@
 # Aca se dibuja el tablero y se piden los mivimientos al usuario, interfaz de la consola
+import os
+
+class UIConsole:
+    @staticmethod
+    def solicitarTamanoTablero():
+        """
+        Pide al usuario que ingrese tamaño del tablero, no debe ser menor a 5x5
+        Uso de try, except para asegurar que ingrese solo numeros enteros
+        """
+        while True:
+            try: 
+                tablero = int(input("Introduce el tamaño del tablero(minimo 5): "))
+                if tablero >= 5:
+                    return tablero
+                else:
+                    print("Tablero muy pequeño, ingrese otro numero")
+            except ValueError:
+                print("Ingrese un numero entero valido")
