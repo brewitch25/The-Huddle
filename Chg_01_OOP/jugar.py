e695f99282cdb3ed7949d9ad5124c79caa7a6cbb
Vanina Maldonado <vanimaldo.vm@gmail.com>
2026-05-20 17:42:48 -0300

    Se crea la clase Game, se agregan las condiciones iniciales del juego, como, la solicitud del tamaño del tablero, la ubicacion de los personajes en el tablero y el estado del juego

--- Chg_01_OOP/jugar.py
+++ Chg_01_OOP/jugar.py
@@ -2,3 +2,32 @@
 import random
 from personajes import Gato, Raton, Queso
 from main import UIConsole
+
+class Game:
+    def __init__(self):
+        """
+        Constructor del terreno de juego
+        """
+        # Solicitud del tamaño de la matriz de juego
+        self.tamaño_tablero =  UIConsole.solicitar_tamano_tablero()
+        
+        # Ubicamos al raton en la esquina inferior derecha
+        limite_maximo = self.tamaño_tablero - 1
+        self.raton = Raton(limite_maximo, limite_maximo)
+
+        # Ubicamos al gato de forma aleatoria en el tablero
+        self.gato = self.inicializar_gato_aleatorio()
+
+        # Ubicamos al queso de forma aleatoria en el tablero(evitando al gato y al raton)
+        self.queso = self.inicializar_queso_aleatorio()
+
+        # Estado inicial del juego
+        self.jugando = True
+
+    def es_posicion_valida(self, x, y):
+        """
+
+        """
+
+
+
