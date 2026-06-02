91792d15e23cf036da079459ec7635371736b7a0
Vanina Maldonado <vanimaldo.vm@gmail.com>
2026-06-02 10:30:55 -0300

    Se creo las constantes para el host y el puerto, tambien se creo la funcion que crea el bradcast con los clientes

--- Chg_02_Sockets/servidor.py
+++ Chg_02_Sockets/servidor.py
@@ -1,2 +1,20 @@
 import socket
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+import threading
+
+# Configuraciones del host y puerto
+HOST = '127.0.0.1'
+PORT = 50001
+
+# Para almacenar sockets de clientes activos
+clientes = []
+
+def crear_broadcast(mensaje, cliente_origen):
+    """
+    Envia mensaje a todos los clientes, excepto al que lo envio
+    """
+    for cliente in cliente:
+        if cliente != cliente_origen:
+            try:
+                cliente.send(mensaje)
+            except:
+                remover_cliente(cliente)    # Si falla al enviar
