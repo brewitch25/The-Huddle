3e11d62dcc4ccc54b40cf01ac714333d473030e9
Vanina Maldonado <vanimaldo.vm@gmail.com>
2026-06-23 19:55:45 -0300

    Se crean las tablas orders y order_items

--- Chg_03_Busq_BD_SQL/creacion.sql
+++ Chg_03_Busq_BD_SQL/creacion.sql
@@ -19,6 +19,32 @@ CREATE TABLE products (
     unit_price decimal(10, 2) NOT NULL,
     unit_cost decimal(10, 2) NOT NULL,
     created_at int NOT NULL,
-    is_active tinyint NOT NULL, 
+    is_active tinyint(1) NOT NULL, 
     deleted_at timestamp
 );
+
+CREATE TABLE orders (
+    order_id int primary key,
+    foreign key (customer_id) NOT NULL,
+    order_datetime timestamp NOT NULL,
+    channel varchar(63) NOT NULL,
+    currency varchar(25) NOT NULL,
+    current_status varchar(25) NOT NULL,
+    order_total decimal (10, 2) NOT NULL,
+    is_active tinyint(1) NOT NULL,
+    deleted_at timestamp
+);
+
+CREATE TABLE order_items (
+    order_item_id int primary key,
+    order_id int not null, 
+    product_id int null,
+    quantity int NOT NULL,
+    unit_price decimal (10, 2) NOT NULL,
+    discount_rate decimal (10, 2) NOT NULL,
+    line_total decimal(10,2) not null,
+    constraint fk_order foreign key (order_id) references orders(order_id),
+    constraint fk_product foreign key (product_id) references products (product_id)
+);
+
+
