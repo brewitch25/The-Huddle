4ed1b75bf2f39cdbd9fa7e6d38e27429b3a11ded
Vanina Maldonado <vanimaldo.vm@gmail.com>
2026-06-23 20:26:04 -0300

    Se crean las tablas payments, order_status_history y order_audit

--- Chg_03_Busq_BD_SQL/creacion.sql
+++ Chg_03_Busq_BD_SQL/creacion.sql
@@ -47,4 +47,36 @@ CREATE TABLE order_items (
     constraint fk_product foreign key (product_id) references products (product_id)
 );
 
+CREATE TABLE payments (
+    payment_id INT primary key,
+    order_id int not null,
+    payment_datetime timestamp not null,
+    method varchar(25) not null,
+    payment_status varchar(25) not null,
+    amound decimal(10, 2) not null,
+    currency varchar(25) not null,
+    constraint fk_order foreign key (order_id) references orders (order_id)
+);
+
+CREATE TABLE order_status_history (
+    status_history_id int primary key,
+    order_id int not null,
+    order_status varchar(25) not null, 
+    changed_at timestamp not null,
+    changed_by varchar(25) not null,
+    reason varchar(25),
+    constraint fk_order foreign key (order_id) references orders (order_id)
+)
+
+
+CREATE TABLE order_audit (
+    audit_id int primary key,
+    order_id int not null,
+    field_name varchar(25) not null,
+    old_value varchar(25) not null,
+    new_value varchar(25) not null,
+    changed_at timestamp not null,
+    changed_by varchar(25) not null,
+    constraint fk_order foreign key (order_id) references orders (order_id)
+)
 
